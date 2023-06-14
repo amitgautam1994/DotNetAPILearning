88ef0a7abad8380b546dae84ffe1b7d8a9f23b03
amitgautam1 <amitgautam@edugorilla.org>
2023-06-14 09:27:51 +0530

    updated

--- DotnetAPI/Data/DataContextDapper.cs
+++ DotnetAPI/Data/DataContextDapper.cs
@@ -7,20 +7,20 @@ namespace DotnetAPI.Data
     class DataContextDapper
     {
         private readonly IConfiguration _config;
-        public DataContextDapper(IConfiguration config) 
+        public DataContextDapper(IConfiguration config)
         {
             _config = config;
         }
 
         public IEnumerable<T> LoadData<T>(string sql)
         {
-            IDbConnection dbConnection  = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             return dbConnection.Query<T>(sql);
         }
 
         public T LoadDataSingle<T>(string sql)
         {
-            IDbConnection dbConnection  = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             return dbConnection.QuerySingle<T>(sql);
         }
 
@@ -34,6 +34,24 @@ namespace DotnetAPI.Data
         {
             IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             return dbConnection.Execute(sql);
+        }
+
+        public bool ExecuteSqlWithParameters(string sql, DynamicParameters parameters)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Execute(sql, parameters) > 0;
+        }
+
+        public IEnumerable<T> LoadDataWithParameters<T>(string sql, DynamicParameters parameters)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Query<T>(sql, parameters);
+        }
+
+        public T LoadDataSingleWithParameters<T>(string sql, DynamicParameters parameters)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.QuerySingle<T>(sql, parameters);
         }
     }
 }
--- DotnetAPI/Program.cs
+++ DotnetAPI/Program.cs
@@ -1,4 +1,8 @@
+using System.Text;
 using DotnetAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,7 +29,20 @@ builder.Services.AddCors((options) => {
     });
 });
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+string? tokenKeyString = builder.Configuration.GetSection("AppSettings:TokenKey").Value;
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters() 
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    tokenKeyString != null ? tokenKeyString : ""
+                )),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
 
 var app = builder.Build();
 
@@ -42,7 +59,7 @@ else
     app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
